a7da5370f48a2c431c78c2ae3ec9eac96b3bbfb9
chef-nic <nicholasajohnson@me.com>
2024-08-01 13:28:30 -0700

    Initial Commit

---
+++ GoodFriend/GoodFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodFriendApp.swift
+//  GoodFriend
+//
+//  Created by Nicholas Johnson on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GoodFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
